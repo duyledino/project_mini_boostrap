ff689b8cb540c089c85551b78db55a7c9969cb07
duyledinodeptrai300cm <duyle@gmail.com>
2024-10-12 19:05:43 +0700

    commit nth

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
+const getSection = document.querySelectorAll("section");
+const getSection2 = document.querySelector(".section2");
 const getMenu = document.querySelector(".header .mobi");
-const getIcon = document.querySelector(".header .inner-wrap .inner-title li:last-child a i");
+const getIcon = document.querySelector(
+  ".header .inner-wrap .inner-title li:last-child a i"
+);
 const getlogo = document.querySelector(".header .inner-wrap");
 let isHomeIcon = true;
-const navIcon = document.getElementById('navIcon');
+const navIcon = document.getElementById("navIcon");
 
 getIcon.addEventListener("click", () => {
-    getlogo.classList.toggle("menu-on-logo");
-    getMenu.classList.toggle("menu-on");
-    navIcon.classList.add('fade-out');
-        // Wait for the fade-out transition to complete
-        setTimeout(() => {
-            // Switch the icon class
-            navIcon.classList.remove('fa-bars-staggered', 'fa-x');
-            navIcon.classList.add(isHomeIcon ? 'fa-x' : 'fa-bars-staggered');
-            isHomeIcon = !isHomeIcon;
+  getlogo.classList.toggle("menu-on-logo");
+  getMenu.classList.toggle("menu-on");
+  navIcon.classList.add("fade-out");
+  // Wait for the fade-out transition to complete
+  setTimeout(() => {
+    // Switch the icon class
+    navIcon.classList.remove("fa-bars-staggered", "fa-x");
+    navIcon.classList.add(isHomeIcon ? "fa-x" : "fa-bars-staggered");
+    isHomeIcon = !isHomeIcon;
 
-            // Remove fade-out and add fade-in class
-            navIcon.classList.remove('fade-out');
-            navIcon.classList.add('fade-in');
+    // Remove fade-out and add fade-in class
+    navIcon.classList.remove("fade-out");
+    navIcon.classList.add("fade-in");
 
-            // Remove fade-in class after transition
-            setTimeout(() => {
-                navIcon.classList.remove('fade-in');
-            }, 300); 
-            // Match this duration with the transition time in CSS
-        }, 300); // Match this duration with the transition time in CSS
+    // Remove fade-in class after transition
+    setTimeout(() => {
+      navIcon.classList.remove("fade-in");
+    }, 300);
+    // Match this duration with the transition time in CSS
+  }, 300); // Match this duration with the transition time in CSS
+});
+
+getSection.forEach(section => {
+    window.addEventListener("scroll",() => {
+        const top = window.scrollY;
+        const offsetT = section.offsetTop - 300;
+        const offsetH = section.offsetHeight + 300;
+        if(top >= offsetT && top < offsetH+offsetT){
+            section.classList.add("animate");
+        }else{
+            section.classList.remove("animate");
+        }
+    })
 })
